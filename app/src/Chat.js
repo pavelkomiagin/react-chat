567d59b3f56c3e34bcabf384f784f0160271b92a
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-10-28 22:55:03 +0300

    Add Chat component

---
+++ app/src/Chat.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class Chat extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>
+                 <div className='top_menu'>
+                   <div className='buttons'>
+                     <div className='button close'></div>
+                     <div className='button minimize'></div>
+                     <div className='button maximize'></div>
+                   </div>
+                   <div className='title'>Chat</div>
+                 </div>
+                 <MessagesList />
+                 <BottomWrapper />
+              </div>
+    }
+}
+
+class MessagesList extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <ul className='messages'></ul>
+    }
+}
+
+class BottomWrapper extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div className='bottom_wrapper clearfix'>
+                 <div className='message_input_wrapper'>
+                   <input type='text' className='message_input' placeholder='Type your message here...' />
+                 </div>
+                 <div className='send_message'>
+                   <div className='icon'></div>
+                   <div className='text'>Send</div>
+                 </div>
+               </div>
+    }
+}
