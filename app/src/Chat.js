e78e0ca292686b65e03a8a20a8df955550a3b515
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-10-28 22:59:00 +0300

    Fix indents

--- app/src/Chat.js
+++ app/src/Chat.js
@@ -4,50 +4,60 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class Chat extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return <div>
-                 <div className='top_menu'>
-                   <div className='buttons'>
-                     <div className='button close'></div>
-                     <div className='button minimize'></div>
-                     <div className='button maximize'></div>
-                   </div>
-                   <div className='title'>Chat</div>
-                 </div>
-                 <MessagesList />
-                 <BottomWrapper />
-              </div>
-    }
+  render() {
+    return <div>
+             <div className='top_menu'>
+               <TopButtons />
+               <div className='title'>Chat</div>
+             </div>
+             <MessagesList />
+             <BottomWrapper />
+           </div>
+  }
+}
+
+class TopButtons extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className='buttons'>
+             <div className='button close'></div>
+             <div className='button minimize'></div>
+             <div className='button maximize'></div>
+           </div>
+  }
 }
 
 class MessagesList extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return <ul className='messages'></ul>
-    }
+  render() {
+    return <ul className='messages'></ul>
+  }
 }
 
 class BottomWrapper extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return <div className='bottom_wrapper clearfix'>
-                 <div className='message_input_wrapper'>
-                   <input type='text' className='message_input' placeholder='Type your message here...' />
-                 </div>
-                 <div className='send_message'>
-                   <div className='icon'></div>
-                   <div className='text'>Send</div>
-                 </div>
-               </div>
-    }
+  render() {
+    return <div className='bottom_wrapper clearfix'>
+             <div className='message_input_wrapper'>
+               <input type='text' className='message_input' placeholder='Type your message here...' />
+             </div>
+             <div className='send_message'>
+               <div className='icon'></div>
+               <div className='text'>Send</div>
+             </div>
+           </div>
+  }
 }
